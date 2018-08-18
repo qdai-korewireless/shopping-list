6ebf5a514e7c43d3c88b83542cae2faa191a006d
qdai-korewireless <qdai@korewireless.com>
2018-08-18 09:23:34 -0700

    1. able to connect to cassandra. 2. add repositories

--- alpha/Controllers/HomeController.cs
+++ alpha/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using alpha.Models;
 using alpha.Services;
+using Cassandra;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -24,8 +25,22 @@ namespace alpha.Controllers
 
             var item = new Item();
             item.Name = "Apple";
-
-
+            var myCassandraCluster = Cluster.Builder()
+                                            .AddContactPoint("localhost")
+    //.WithAuthProvider(new DsePlainTextAuthProvider("username", "password"))
+    .Build();
+            using (var session = myCassandraCluster.Connect("shoppingcart"))
+            {
+               
+                var query = new SimpleStatement("SELECT id, name, quantity, typeid FROM item where id = ?", 1);
+                var rs =  session.Execute(query);
+                var row = rs.FirstOrDefault();
+                if (row != null)
+                {
+                    var itemName = row.GetValue(typeof(string), 1);
+                    item.Name = itemName.ToString();
+                }
+            }
             return View(item);
         }
 
