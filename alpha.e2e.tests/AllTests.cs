956e73fa9c9fa70f67d9d4c13347a6fccda26a13
qdai-korewireless <qdai@korewireless.com>
2018-09-01 21:16:05 -0700

    add new test for dish page to test add new dish; also have to move dish test to one test class due to conflict in IMapper key/value when using saperate factory in saperate classes

--- alpha.e2e.tests/AllTests.cs
+++ alpha.e2e.tests/AllTests.cs
@@ -5,6 +5,8 @@ using alpha;
 using System.Threading.Tasks;
 using FluentAssertions;
 using alpha.e2e.tests.Helpers;
+using System.Collections.Generic;
+using AngleSharp.Dom.Html;
 
 namespace alpha.e2e.tests
 {
@@ -51,7 +53,43 @@ namespace alpha.e2e.tests
             addDishUrl.Should().Be("/Dish/Index");
 
         }
+        [Theory]
+        [InlineData("/Dish/Index")]
+        public async Task Get_DishIndex_StatusOK(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
 
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            response.Content.Headers.ContentType.ToString().Should().Be("text/html; charset=utf-8");
+
+        }
+
+        [Theory]
+        [InlineData("/Dish/Index")]
+        public async Task Index_CanSubmitDish_OnDishPage(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+            var content = await HtmlHelpers.GetDocumentAsync(response);
+            var frm = content.QuerySelector("#frmSaveDish");
+            var frmData = new List<KeyValuePair<string, string>>() { KeyValuePair.Create<string, string>("Name", "e2eTest") };
+            var addDishResponse = await client.SendAsync((IHtmlFormElement)frm, frmData);
+            content = await HtmlHelpers.GetDocumentAsync(addDishResponse);
+            var dishIdEle = (IHtmlInputElement)content.QuerySelector("#dishId");
+            var dishId = new Guid(dishIdEle.Value);
+            // Assert
+
+            dishId.Should().NotBe(Guid.Empty);
+
+        }
 
     }
 }
